245e7eadc05dd803244a90a16000dc9200c1b520
Quise <darkquise@gmail.com>
2023-02-18 11:47:00 -0500

    made object called myPasswordGenerator and added some initial values

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,20 @@
 // Assignment code here
+const specialCharacters = ['\\','!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+console.log(specialCharacters)
+console.log(specialCharacters[0])
 
+const myPasswordGenerator = {
+  _numbers: ['0','1','2','3','4','5','6','7','8','9'],
+  _lowerCaseAlphabets: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  _upperCaseAlphabets: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+  _specialCharacters: ['\\','!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'],
+  _usingNumbers: true,
+  _usingLowerCase: true,
+  _usingUpperCase: true,
+  _usingSpecials: true
 
+  
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +29,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(myPasswordGenerator.getNumbers())
